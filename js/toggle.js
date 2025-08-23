14daf9dba82c65240d7a500461ac197be4f0c783
MatteoPedone <matteopedone10@gmail.com>
2025-08-23 16:49:56 +0200

    MPS-0001 [pedone]: create header

---
+++ js/toggle.js
@@ -0,0 +1,34 @@
+// Carica la configurazione dal file JSON
+fetch('config.json')
+    .then(res => res.json())
+    .then(config => {
+        const isActive = config.isSiteActive;
+
+        const maintenance = document.getElementById('maintenance');
+        const main = document.getElementById('main');
+
+        // Mostra la sezione corretta
+        if (isActive) {
+            maintenance.style.display = 'none';
+            main.style.display = 'block';
+
+            // Carica i componenti modulari
+            loadComponent('components/hero-header.html', 'hero-header');
+            loadComponent('components/hero.html', 'hero');
+
+            //loadComponent('components/contact.html', 'contact');
+            //loadComponent('components/footer.html', 'footer');
+        } else {
+            maintenance.style.display = 'flex';
+            main.style.display = 'none';
+        }
+    });
+
+// Funzione per caricare componenti HTML
+function loadComponent(url, targetId) {
+    fetch(url)
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById(targetId).innerHTML = html;
+        });
+}
